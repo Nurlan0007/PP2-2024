4c3959159767499bb0b16dc3ede67dc07f1751ec
Nurlan <tlek.nurlan06@gmail.com>
2024-03-07 15:44:22 +0500

    Lab6 changes

--- Lab6/built-in_func/5.py
+++ Lab6/built-in_func/5.py
@@ -2,10 +2,12 @@ def check_all_true(t):
     return all(t)
 
 # Test the function
-t1 = (True, True, True)
-t2 = (True, False, True)
-t3 = ()
+t1 = (1, 1, 1)
+t2 = (True, False,True)
+t3 = (True, True)
+t4 = ()
 
 print(check_all_true(t1))  
 print(check_all_true(t2))  
-print(check_all_true(t3))  
+print(check_all_true(t3))
+print(check_all_true(t4))   
